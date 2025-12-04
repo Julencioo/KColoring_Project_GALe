b73000da06458ed9d23609a36ac82ccb24d81bc1
Eduardo <emp.edu13@gmail.com>
2025-12-04 11:45:23 +0100

    edu- DSATUR.h first update

--- include/DSATUR.h
+++ include/DSATUR.h
@@ -2,7 +2,23 @@
 // Created by Eduardo Martín Postigo (xmartie01) on 04/12/2025.
 //
 
-#ifndef KCOLORING_PROJECT_GALE_DSATUR_H
-#define KCOLORING_PROJECT_GALE_DSATUR_H
+#ifndef DSATUR_H
+#define DSATUR_H
 
-#endif //KCOLORING_PROJECT_GALE_DSATUR_H
+#include "Graph.h"
+#include <vector>
+
+// Struct to hold the results for experimentation/documentation
+struct ColoringResult {
+    int chromatic_number;        // The 'k' in k-coloring (quality of solution)
+    std::vector<int> assignment; // assignment[v] = color
+};
+
+class DSatur {
+public:
+    // Solves the graph coloring using the DSATUR heuristic
+    // Returns the number of colors used and the specific assignment
+    ColoringResult solve(const Graph& g);
+};
+
+#endif //DSATUR_H
